6dd34bbe495a31cff8bdd7814cd65053edab1739
Xinyang Li <112905845+XinyangLi1@users.noreply.github.com>
2025-03-13 16:41:27 -0500

    fix oauth, added text input

---
+++ demo_Xinyang_Li_Week2/django-demo-project/my_demo_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import UserTextData
+
+class UserTextDataForm(forms.ModelForm):
+    class Meta:
+        model = UserTextData
+        fields = ['name', 'text_data']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Enter your name'}),
+            'text_data': forms.Textarea(attrs={'rows': 10, 'cols': 50, 'id': 'editor'}),
+        }
---
+++ demo_Xinyang_Li_Week2/django-demo-project/my_demo_app/pipeline.py
@@ -0,0 +1,34 @@
+# filepath: e:\cs222\team-00-project\demo_Xinyang_Li_Week2\django-demo-project\my_demo_app\pipeline.py
+from social_core.exceptions import AuthAlreadyAssociated
+from django.contrib.auth import logout
+from .models import LoggedInUser
+
+def social_user(backend, uid, user=None, *args, **kwargs):
+    provider = backend.name
+    social = backend.strategy.storage.user.get_social_auth(provider, uid)
+
+    if social:
+        if user and social.user != user:
+            msg = 'This account is already in use.'
+            raise AuthAlreadyAssociated(backend, msg)
+
+        if not user:
+            user = social.user
+
+        # Log out the currently logged-in user if there is one
+        try:
+            current_logged_in_user = LoggedInUser.objects.get()
+            logout(backend.strategy.request)
+            current_logged_in_user.delete()
+        except LoggedInUser.DoesNotExist:
+            pass
+
+        # Manually set the backend attribute on the user object
+        user.backend = f'social_core.backends.{provider}.{provider.capitalize()}OAuth2'
+
+        # Log in the new user and update the LoggedInUser model
+        backend.strategy.session['_auth_user_id'] = user.pk
+        backend.strategy.session['_auth_user_backend'] = user.backend
+        LoggedInUser.objects.create(user=user)
+
+    return {'social': social, 'user': user}
