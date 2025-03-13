c365a9ebb77fd8ce99ec2e0c2009ca9b2b4d6321
Xinyang Li <112905845+XinyangLi1@users.noreply.github.com>
2025-03-13 16:54:39 -0500

    2

--- demo_Xinyang_Li_Week2/django-demo-project/my_demo_app/admin.py
+++ demo_Xinyang_Li_Week2/django-demo-project/my_demo_app/admin.py
@@ -1,5 +1,5 @@
-# filepath: e:\cs222\team-00-project\demo_Xinyang_Li_Week2\django-demo-project\my_demo_app\admin.py
 from django.contrib import admin
-from .models import LoggedInUser
+from .models import LoggedInUser, UserTextData
 
 admin.site.register(LoggedInUser)
+admin.site.register(UserTextData)
